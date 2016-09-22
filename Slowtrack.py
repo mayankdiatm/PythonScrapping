7550a403ca53d857d4a7f72268b07f3fdd148351
mayankdiatm <mayankdiatm@gmail.com>
2016-09-22 15:31:34 +0530

    Update Slowtrack.py

--- Slowtrack.py
+++ Slowtrack.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-res = requests.get('http://spvss-gpk-bot01.cisco.com:10082/tags/STB_SW.git?branch=master')
+res = requests.get('')
 type(res)
 res.status_code
 print(res.text[:1000])
